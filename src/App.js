633a9746f48d222c779746b8f0bf40fb7325d309
AxiomaAbsurdo <m.mortara01@gmail.com>
2021-11-23 20:18:04 -0300

    add: new example repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from '@mantine/core';
+import React from "react";
+
+function App(){
+
+    return (
+        <Button> Follow on Twitter
+        </Button>
+    );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<App/>,document.querySelector("#root"));
+
+// const persona = {
+//     nombre: 'Matias',
+//     apellido: 'Mortara',
+//     datosPersonales: {
+//         edad: 34,
+//         sexo: 'masculino',
+//     }
+// }
+// console.table(persona);
